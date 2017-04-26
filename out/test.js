b1a2339bbcebe2d7f6c7cfd400217aa64437d9db
613S023\sse <599318583@qq.com>
2017-04-26 15:08:00 +0800

    Finish Homework......

--- out/test.js
+++ out/test.js
@@ -1,9 +1,26 @@
-window.onload = function () {
-    var canvas = document.getElementById("app");
-    var stage = engine.run(canvas);
-    var bitmap = new engine.Bitmap();
-    var image = document.createElement("img");
-    image.src = "wander-icon.jpg";
-    bitmap.image = image;
-    stage.addChild(bitmap);
-};
+"use strict";
+var canvas = document.getElementById("app");
+var stage = cadence.run(canvas);
+var touchService = cadence.TouchEventService.getInstance();
+// var bitmap = new cadence.Bitmap();
+// cadence.RES.getRes("0008.png").then((value) => {
+//     bitmap.texture = value;
+//     bitmap.setWidth(bitmap.texture.width);
+//     bitmap.setHeight(bitmap.texture.height);
+// });
+// stage.addChild(bitmap);
+// let speed = 190;
+// cadence.Ticker.getInstance().register((deltaTime) => {
+//     //console.log("aaa");
+//     bitmap.setWidth(500);
+// });
+var main = new Main(stage, touchService);
+cadence.setMain(main);
+// cadence.RES.getRES("RES.json",(data) => {
+//     var preloadJson = data;
+//     cadence.RES.loadConfig(preloadJson, () => {
+//         console.log("load complete!!!");
+//         var main = new Main(stage,touchService);
+//         main.createGameScene();
+//     });
+// });
